2d3386a8a26118c911f2e2bdc9f10a31ea841082
Ntkskwk <natsukawa247@outlook.com>
2025-10-04 19:31:00 +0800

    feat(app): add suspense wrapper for HelloPage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 
+import { Suspense } from "react";
 import HelloPage from "../components/page";
+
+function HelloPageWrapper() {
+  return <HelloPage />;
+}
 
 export default function Home() {
   return (
     <>
-      <HelloPage />
+      <Suspense fallback={<div>Loading...</div>}>
+        <HelloPageWrapper />
+      </Suspense>
     </>
   );
 }
