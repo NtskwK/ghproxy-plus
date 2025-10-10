d395b4c3f51f2d989d3a501c03a7a97de139f523
Ntkskwk <natsukawa247@outlook.com>
2025-10-10 16:48:26 +0800

    chore(not-found): enhance 404 page UI and functionality

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,17 @@
-import Link from "next/link";
+"use client";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function NotFound() {
+  const router = useRouter();
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex flex-col items-center justify-center text-center gap-4 w-full min-h-[60vh]">
+      <h1 className="text-6xl font-bold">404</h1>
+      <p>
+        <span className="text-red-500 font-bold text-2xl">Oops!</span> The page
+        you requested doesn't exist.
+      </p>
+      <Button onClick={() => router.push("/")}>Go Home</Button>
     </div>
   );
 }
