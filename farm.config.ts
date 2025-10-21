77eaaacf2dd14fa3367bff38e9f551bc8ef7e432
NtskwK <natsukawa247@outlook.com>
2025-10-21 11:24:43 +0800

    feat: add Hono dev server plugin

--- farm.config.ts
+++ farm.config.ts
@@ -1,10 +1,18 @@
 import path from "node:path";
 import { defineConfig } from "@farmfe/core";
 import farmJsPluginPostcss from "@farmfe/js-plugin-postcss";
+import devserver from "@hono/vite-dev-server"
+
+function honoVitePlugin() {
+  return {
+    name: "hono-devserver-plugin",
+    plugin: devserver,
+  }
+}
 
 export default defineConfig({
   plugins: ["@farmfe/plugin-react", farmJsPluginPostcss()],
-  vitePlugins: [],
+  vitePlugins: [honoVitePlugin()],
   compilation: {
     resolve: {
       alias: {
